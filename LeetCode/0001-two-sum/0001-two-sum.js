bb00d0b9c9a607ddd0e084af0fef267c2468cdcd
yiseungyun <73896327+yiseungyun@users.noreply.github.com>
2024-04-27 21:22:07 +0900

    Time: 66 ms (54.12%), Space: 51.8 MB (16.57%) - LeetHub

--- LeetCode/0001-two-sum/0001-two-sum.js
+++ LeetCode/0001-two-sum/0001-two-sum.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length-1; i++) {
-        for (let j = i+1; j < nums.length + 1; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j]
-            }
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (!map.has(nums[i])) map.set(nums[i], i)
+        if (map.has(target-nums[i]) && map.get(target-nums[i]) !== i) {
+            return [map.get(target-nums[i]), i]
         }
     }
 };
