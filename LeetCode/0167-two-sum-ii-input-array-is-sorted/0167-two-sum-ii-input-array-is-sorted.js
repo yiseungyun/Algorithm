72d65e960465ee92abe43cd27ead51247882860d
yiseungyun <73896327+yiseungyun@users.noreply.github.com>
2024-05-11 15:54:55 +0900

    Time: 45 ms (96%), Space: 49.2 MB (96.75%) - LeetHub

--- LeetCode/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ LeetCode/0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,12 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    var dictionary = {};
-    for (let i = 0; i < numbers.length; i++) {
-        if (target-numbers[i] in dictionary) {
-            return [dictionary[target-numbers[i]], i+1];
+    var left = 0, right = numbers.length-1;
+    while (1) {
+        if (numbers[left]+numbers[right] === target) {
+            return [left+1, right+1];
+        } else if (numbers[left]+numbers[right] > target) {
+            right -= 1;
         } else {
-            dictionary[numbers[i]] = i+1;
+            left += 1;
         }
     }
 };
