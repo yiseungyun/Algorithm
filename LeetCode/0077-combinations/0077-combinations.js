df33ec8408921e0195f018f81d7089c557f52e0d
yiseungyun <73896327+yiseungyun@users.noreply.github.com>
2024-05-18 16:14:42 +0900

    Time: 602 ms (6.83%), Space: 150 MB (19.19%) - LeetHub

---
+++ LeetCode/0077-combinations/0077-combinations.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    var result = [];
+    function dfs(list) {
+        if (list.length === k) {
+            result.push(list);
+            return;
+        }
+        for (let i = 1; i < n+1; i++) {
+            if (list[list.length-1] < i || list.length === 0) {
+                dfs([...list, i]);
+            }
+        }
+    }
+    dfs([])
+    return result;
+};
