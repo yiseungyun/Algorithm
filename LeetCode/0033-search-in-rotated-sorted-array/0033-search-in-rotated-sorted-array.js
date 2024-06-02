fe6b316207c2334d0c93151f250e937aed63ce1a
yiseungyun <73896327+yiseungyun@users.noreply.github.com>
2024-06-02 14:31:34 +0900

    Time: 55 ms (44.87%), Space: 49.2 MB (26.05%) - LeetHub

--- LeetCode/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ LeetCode/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -5,34 +5,34 @@
  */
 var search = function(nums, target) {
     function start_search(start, end) {
-        var mid = Math.floor((start+end)/2);
         if (start >= end) return end;
-        if (nums[start] < nums[end]) {
-            return start;
-        }
+        var mid = Math.floor((start+end)/2);
         if (nums[start] > nums[mid]) {
-            return start_search(start, mid);
-        } else {
+            return start_search(start+1, mid);
+        } else if (nums[mid] > nums[end]) {
             return start_search(mid+1, end);
+        } else {
+            return start;
         }
     }
     var start_index = start_search(0, nums.length-1);
     function binary_search(start, end) {
+        if (start >= end && nums[end] === target) return end;
+        else if (start >= end) return -1;
         var mid = Math.floor((start+end)/2);
-        if (start > end) return -1;
-        if (nums[mid] === target) {
-            return mid;
-        }
-        if (nums[mid] > target) {
+        if (target > nums[mid]) {
+            return binary_search(mid+1, end);
+        } else if (target < nums[mid]) {
             return binary_search(start, mid-1);
         } else {
-            return binary_search(mid+1, end);
+            return mid;
         }
     }
-
-    var first = binary_search(0, start_index-1);
-    var second = binary_search(start_index, nums.length-1);
-    if (first === -1 && second === -1) return -1;
-    else if (first > -1) return first;
-    else return second;
+    if (nums[nums.length-1] > target) {
+        return binary_search(start_index, nums.length-1);
+    } else if (nums[nums.length-1] < target) {
+        return binary_search(0, start_index-1);
+    } else {
+        return nums.length-1;
+    }
 };
