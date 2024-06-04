a10d0921105b48c47284fd0870d327c1e1322fe1
yiseungyun <73896327+yiseungyun@users.noreply.github.com>
2024-06-04 17:56:47 +0900

    Time: 62 ms (6.24%), Space: 49.4 MB (5.39%) - LeetHub

--- LeetCode/0070-climbing-stairs/0070-climbing-stairs.js
+++ LeetCode/0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    var DP = new Array(n+1);
-    DP[0] = 1;
+    var DP = Array.from({length: n+1}, (_, i) => 0);
     DP[1] = 1;
-    for (let i = 2; i < n+1; i++) {
-        DP[i] = DP[i-2]+DP[i-1];
+    DP[2] = 2;
+    for (let i = 3; i < n+1; i++) {
+        DP[i] = DP[i-2] + DP[i-1];
     }
     return DP[n];
 };
