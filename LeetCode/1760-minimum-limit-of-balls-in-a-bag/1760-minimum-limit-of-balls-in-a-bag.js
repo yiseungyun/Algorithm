21d90582ec3854fbc62f6360e7cd9348ad57a6bf
yiseungyun <73896327+yiseungyun@users.noreply.github.com>
2024-12-29 19:07:10 +0900

    Time: 39 ms (54.39%), Space: 60.7 MB (47.09%) - LeetHub

--- LeetCode/1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.js
+++ LeetCode/1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.js
@@ -4,30 +4,23 @@
  * @return {number}
  */
 const minimumSize = function(nums, maxOperations) {
-    let left = 1;
-    let right = Math.max(...nums);
+    let left = 1, right = Math.max(...nums);
     let result = right;
 
     while (left <= right) {
+        let operation = 0;
         const mid = Math.floor((left+right)/2);
+        for (const num of nums) {
+            operation += Math.floor((num-1)/mid);
+        }
 
-        if (isPenaltyPossible(nums, maxOperations, mid)) {
+        if (operation <= maxOperations) {
             result = mid;
-            right = mid-1; 
+            right = mid-1;
         } else {
-            left = mid + 1;
+            left = mid+1;
         }
     }
 
     return result;
 };
-
-const isPenaltyPossible = (nums, maxOperations, mid) => {
-    let operations = 0;
-    for (const num of nums) {
-        operations += Math.floor((num-1)/mid);
-        if (operations > maxOperations) return false;
-    }
-
-    return true;
-}
