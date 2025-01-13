0d7729af1aad29c60eb8523a74a8718a35428d3e
yiseungyun <73896327+yiseungyun@users.noreply.github.com>
2025-01-14 01:28:25 +0900

    Time: 2 ms (45.39%), Space: 52.5 MB (37.95%) - LeetHub

--- LeetCode/0042-trapping-rain-water/0042-trapping-rain-water.js
+++ LeetCode/0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -4,17 +4,19 @@
  */
 var trap = function(height) {
     let result = 0;
-    let stack = [];
+    let left = 0, right = height.length-1;
+    let leftMax = height[left], rightMax = height[right];
 
-    for (let i = 0; i < height.length; i++) {
-        while (stack.length > 0 && height[stack[stack.length-1]] < height[i]) {
-            const top = stack.pop();
-            if (stack.length === 0) break;
-            const width = i-stack[stack.length-1]-1;
-            const minHeight = Math.min(height[i], height[stack[stack.length-1]])-height[top];
-            result += width*minHeight;
+    while (left <= right) {
+        if (leftMax < rightMax) {
+            result += (leftMax-height[left] > 0 ? leftMax-height[left] : 0);
+            left++;
+            leftMax = leftMax < height[left] ? height[left] : leftMax; 
+        } else {
+            result += (rightMax-height[right] > 0 ? rightMax-height[right] : 0);
+            right--;
+            rightMax = rightMax < height[right] ? height[right] : rightMax; 
         }
-        stack.push(i);
     }
 
     return result;
