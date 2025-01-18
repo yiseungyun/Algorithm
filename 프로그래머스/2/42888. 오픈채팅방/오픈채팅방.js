86fd1ffff28f1eb0a9c550b2822f637cb999d4e2
yiseungyun <73896327+yiseungyun@users.noreply.github.com>
2025-01-18 18:12:36 +0900

    [level 2] Title: 오픈채팅방, Time: 302.75 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,31 +1,36 @@
 function solution(record) {
-    let IN = "님이 들어왔습니다.", OUT = "님이 나갔습니다.";
-    let user = {};
     let result = [];
+    const user = {};
     
-    for (let i = 0; i < record.length; i++) {
-        let split_list = record[i].split(" ");
-        let state, id, name;
-        if (split_list.length === 2) {
-            state = split_list[0];
-            id = split_list[1];
+    function message(name, status) {
+        if (status === "IN") {
+            return name + "님이 들어왔습니다."
         } else {
-            state = split_list[0];
-            id = split_list[1];
-            name = split_list[2];
-        }
-        if (state === "Enter") {
-            user[id] = name;
-            result.push(id+IN);
-        } else if (state === "Leave") {
-            result.push(id+OUT);
-        } else if (state === "Change") {
-            user[id] = name;
+            return name + "님이 나갔습니다."
         }
     }
+    
+    for (const r of record) {
+        const recordList = r.split(" ");
+        
+        switch (recordList[0]) {
+            case "Enter":
+                result.push("IN " + recordList[1]);
+                user[recordList[1]] = recordList[2];
+                break;
+            case "Leave":
+                result.push("OUT " + recordList[1]);
+                break;
+            case "Change":
+                user[recordList[1]] = recordList[2];
+                break;
+        }
+    }
+    
     for (let i = 0; i < result.length; i++) {
-        let [id, state] = result[i].split("님");
-        result[i] = user[id] + "님" + state;
+        const [status, id] = result[i].split(" ");
+        result[i] = message(user[id], status); 
     }
+    
     return result;
 }
