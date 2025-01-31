679cb8e97813ea3c401fcaf354ca4a2718b9cb4a
yiseungyun <73896327+yiseungyun@users.noreply.github.com>
2025-01-31 15:30:51 +0900

    [Silver III] Title: 고양이 카페, Time: 168 ms, Memory: 10128 KB -BaekjoonHub

--- 백준/Silver/28353. 고양이 카페/고양이 카페.js
+++ 백준/Silver/28353. 고양이 카페/고양이 카페.js
@@ -8,27 +8,28 @@ let input = [];
 readline.on('line', (line) => {
   input.push(line);
 }).on('close', () => {
-  const [N, K] = input[0].split(" ").map(Number);
-  const weights = input[1].split(" ").map(Number);
-  const result = solution(N, K, weights);
+  const result = solution(input);
   console.log(result);
   process.exit();
 });
 
-const solution = (N, K, weights) => {
-  weights.sort((a, b) => a - b);
-  let count = 0;
+const solution = (input) => {
+  const [N, K] = input[0].split(" ").map(Number);
+  let cats = input[1].split(" ").map(Number);
+  let result = 0;
 
-  let left = 0, right = N - 1;
+  cats.sort((a, b) => a - b);
+
+  let left = 0, right = cats.length-1;
   while (left < right) {
-    if (weights[left] + weights[right] <= K) {
-      count++;
-      left++;
-      right--;
+    const sum = cats[left] + cats[right];
+    if (sum <= K) {
+      result++;
+      left++; right--;
     } else {
       right--;
     }
   }
 
-  return count;
+  return result;
 }
